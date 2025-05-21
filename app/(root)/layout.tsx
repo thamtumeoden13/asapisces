f3e756c28704f7c521734f2b943087b00da937cc
BlackCat13 <detective.blackcat13@gmail.com>
2025-05-21 21:20:26 +0700

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,17 +2,22 @@ import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ContactButton } from "@/components/shared/ContactButton";
+import Hero from "@/components/Hero";
+import About2 from "@/components/About2";
 
-export default function Layout({children}: Readonly<{
-  children: React.ReactNode
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-
   return (
     <main className={"font-ibm-plex"}>
       <Header />
+      <Hero />
+      <About2 />
       {children}
       <ContactButton />
       <Footer />
     </main>
-  )
+  );
 }
