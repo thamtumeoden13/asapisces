887489305f343462282e77763f1fd6edaf9c79f9
BlackCat13 <detective.blackcat13@gmail.com>
2025-06-01 16:41:25 +0700

    update Collaboration

--- app/(root)/brainwave/page.tsx
+++ app/(root)/brainwave/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import React from "react";
 import Hero from "@/components/brainwave/Hero";
 import Benefits from "@/components/brainwave/Benefits";
-import React from "react";
+import Collaboration from "@/components/brainwave/Collaboration";
+import ButtonGradient from "@/components/svg/ButtonGradient";
 
 const page = () => {
   return (
+    <>
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
       <Hero />
       <Benefits />
+      <Collaboration />
     </div>
+    <ButtonGradient />
+
+    </>
   );
 };
 
